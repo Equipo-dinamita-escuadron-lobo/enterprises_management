284d30530ed8768d391c5222f687f19677d69dea
Brayan Majin Gaviria <brayannmajin@gmail.com>
2024-03-10 12:57:22 -0500

    Creation of script for departments and cities

---
+++ src/main/resources/initDatabase.sql
@@ -0,0 +1,43 @@
+INSERT INTO country_entity(id,name)VALUE(1,'Colombia');
+
+INSERT INTO department_entity(id,name)VALUE(1,'Amazonas');
+
+INSERT INTO city_entity (id, name) VALUES (1,'Leticia');
+INSERT INTO city_entity (id, name) VALUES (2,'El Encanto');
+INSERT INTO city_entity (id, name) VALUES (3,'La Chorrera');
+INSERT INTO city_entity (id, name) VALUES (4,'La Pedrera');
+
+INSERT INTO department_entity(id,name)VALUE(2,'Antioquia');
+
+INSERT INTO city_entity (id, name) VALUES (4,'La Pedrera');
+
+INSERT INTO department_entity(id,name)VALUE(3,'Arauca');
+INSERT INTO department_entity(id,name)VALUE(4,'Atlántico');
+INSERT INTO department_entity(id,name)VALUE(5,'Bolivar');
+INSERT INTO department_entity(id,name)VALUE(6,'Boyacá');
+INSERT INTO department_entity(id,name)VALUE(7,'Caldas');
+INSERT INTO department_entity(id,name)VALUE(8,'Caquetá');
+INSERT INTO department_entity(id,name)VALUE(9,'Casanare');
+INSERT INTO department_entity(id,name)VALUE(10,'Cauca');
+INSERT INTO department_entity(id,name)VALUE(11,'Cesar');
+INSERT INTO department_entity(id,name)VALUE(12,'Choco');
+INSERT INTO department_entity(id,name)VALUE(13,'Cordoba');
+INSERT INTO department_entity(id,name)VALUE(14,'Cundinamarca');
+INSERT INTO department_entity(id,name)VALUE(15,'Guainia');
+INSERT INTO department_entity(id,name)VALUE(16,'Guaviare');
+INSERT INTO department_entity (id, name) VALUES (17,'Huila');
+INSERT INTO department_entity (id, name) VALUES (18,'La Guajira');
+INSERT INTO department_entity (id, name) VALUES (19,'Magdalena');
+INSERT INTO department_entity (id, name) VALUES (20,'Meta');
+INSERT INTO department_entity (id, name) VALUES (21,'Nariño');
+INSERT INTO department_entity (id, name) VALUES (22,'Norte de santander');
+INSERT INTO department_entity (id, name) VALUES (23,'Putumayo');
+INSERT INTO department_entity (id, name) VALUES (24,'Quindio');
+INSERT INTO department_entity (id, name) VALUES (25,'Risaralda');
+INSERT INTO department_entity (id, name) VALUES (26,'San Andres y Providencia');
+INSERT INTO department_entity (id, name) VALUES (27,'Santander');
+INSERT INTO department_entity (id, name) VALUES (28,'Sucre');
+INSERT INTO department_entity (id, name) VALUES (29,'Tolima');
+INSERT INTO department_entity (id, name) VALUES (30,'Valle del Cauca');
+INSERT INTO department_entity (id, name) VALUES (31,'Vaupes');
+INSERT INTO department_entity (id, name) VALUES (32,'Vichada');
