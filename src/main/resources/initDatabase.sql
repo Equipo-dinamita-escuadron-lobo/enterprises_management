9e094bcc4fa3a7a977bd34868186650e48586694
Brayan Majin Gaviria <brayannmajin@gmail.com>
2024-03-10 22:08:31 -0500

    city ​​creation

--- src/main/resources/initDatabase.sql
+++ src/main/resources/initDatabase.sql
@@ -1,30 +1,110 @@
 INSERT INTO country_entity(id,name)VALUE(1,'Colombia');
 
-INSERT INTO department_entity(id,name)VALUE(1,'Amazonas');
+INSERT INTO department_entity(id,country_id,name)VALUE(1,'Amazonas');
 
-INSERT INTO city_entity (id, name) VALUES (1,'Leticia');
+INSERT INTO city_entity (id, department_id,name) VALUES (1,'Leticia');
 INSERT INTO city_entity (id, name) VALUES (2,'El Encanto');
 INSERT INTO city_entity (id, name) VALUES (3,'La Chorrera');
 INSERT INTO city_entity (id, name) VALUES (4,'La Pedrera');
 
 INSERT INTO department_entity(id,name)VALUE(2,'Antioquia');
 
-INSERT INTO city_entity (id, name) VALUES (4,'La Pedrera');
+INSERT INTO city_entity (id, name) VALUES (5,'Medellin');
+INSERT INTO city_entity (id, name) VALUES (6,'Bello');
+INSERT INTO city_entity (id, name) VALUES (7,'Itagui');
+INSERT INTO city_entity (id, name) VALUES (8,'Envigado');
+INSERT INTO city_entity (id, name) VALUES (9,'Rionegro');
+
 
 INSERT INTO department_entity(id,name)VALUE(3,'Arauca');
+INSERT INTO city_entity (id, name) VALUES (10,'Arauca');
+
+
 INSERT INTO department_entity(id,name)VALUE(4,'Atlántico');
+INSERT INTO city_entity (id, name) VALUES (11,'Barranquilla');
+INSERT INTO city_entity (id, name) VALUES (12,'Soledad');
+INSERT INTO city_entity (id, name) VALUES (13,'Puerto Colombia');
+
 INSERT INTO department_entity(id,name)VALUE(5,'Bolivar');
+INSERT INTO city_entity (id, name) VALUES (14,'Cartagena de Indias');
+INSERT INTO city_entity (id, name) VALUES (15,'Magangue');
+INSERT INTO city_entity (id, name) VALUES (16,'El carmen de Bolivar');
+INSERT INTO city_entity (id, name) VALUES (17,'MOmpox');
+
 INSERT INTO department_entity(id,name)VALUE(6,'Boyacá');
+INSERT INTO city_entity (id, name) VALUES (18,'Tunja');
+INSERT INTO city_entity (id, name) VALUES (19,'Sogamoso');
+INSERT INTO city_entity (id, name) VALUES (20,'Duitama');
+
 INSERT INTO department_entity(id,name)VALUE(7,'Caldas');
+INSERT INTO city_entity (id, name) VALUES (21,'Manizales');
+INSERT INTO city_entity (id, name) VALUES (22,'La Dorada');
+INSERT INTO city_entity (id, name) VALUES (23,'Chinchina');
+INSERT INTO city_entity (id, name) VALUES (24,'Aguadas');
+INSERT INTO city_entity (id, name) VALUES (25,'Filadelfia');
+
 INSERT INTO department_entity(id,name)VALUE(8,'Caquetá');
+INSERT INTO city_entity (id, name) VALUES (26,'Florencia');
+INSERT INTO city_entity (id, name) VALUES (27,'morelia');
+
 INSERT INTO department_entity(id,name)VALUE(9,'Casanare');
+INSERT INTO city_entity (id, name) VALUES (28,'Yopal');
+INSERT INTO city_entity (id, name) VALUES (29,'Aguazul');
+INSERT INTO city_entity (id, name) VALUES (30,'Chámeza');
+INSERT INTO city_entity (id, name) VALUES (31,'Hato Corozal');
+INSERT INTO city_entity (id, name) VALUES (32,'La Salina');
+
 INSERT INTO department_entity(id,name)VALUE(10,'Cauca');
+INSERT INTO city_entity (id, name) VALUES (33,'Popayán');
+INSERT INTO city_entity (id, name) VALUES (34,'Santander de Quilichao');
+INSERT INTO city_entity (id, name) VALUES (35,'Patía');
+INSERT INTO city_entity (id, name) VALUES (36,'Puerto Tejada');
+
 INSERT INTO department_entity(id,name)VALUE(11,'Cesar');
+INSERT INTO city_entity (id, name) VALUES (37,'Valledupar');
+INSERT INTO city_entity (id, name) VALUES (38,'Aguachica');
+INSERT INTO city_entity (id, name) VALUES (39,'Aguazul');
+INSERT INTO city_entity (id, name) VALUES (40,'Pelaya');
+INSERT INTO city_entity (id, name) VALUES (41,'Bosconia');
+
 INSERT INTO department_entity(id,name)VALUE(12,'Choco');
+INSERT INTO city_entity (id, name) VALUES (42,'Quibdó');
+INSERT INTO city_entity (id, name) VALUES (43,'Acandí');
+INSERT INTO city_entity (id, name) VALUES (44,'Alto Baudó');
+INSERT INTO city_entity (id, name) VALUES (45,'Atrato');
+INSERT INTO city_entity (id, name) VALUES (46,'Bagadó');
+
+
 INSERT INTO department_entity(id,name)VALUE(13,'Cordoba');
+INSERT INTO city_entity (id, name) VALUES (47,'Córdoba');
+INSERT INTO city_entity (id, name) VALUES (48,'Puente Genil');
+INSERT INTO city_entity (id, name) VALUES (49,'Lucena');
+INSERT INTO city_entity (id, name) VALUES (50,'Montilla');
+INSERT INTO city_entity (id, name) VALUES (51,'Priego de Córdoba');
+
 INSERT INTO department_entity(id,name)VALUE(14,'Cundinamarca');
+INSERT INTO city_entity (id, name) VALUES (52,'Bogotá');
+INSERT INTO city_entity (id, name) VALUES (53,'Soacha');
+INSERT INTO city_entity (id, name) VALUES (54,'Zipaquirá');
+INSERT INTO city_entity (id, name) VALUES (55,'Chía');
+INSERT INTO city_entity (id, name) VALUES (56,'Facatativá');
+
 INSERT INTO department_entity(id,name)VALUE(15,'Guainia');
+INSERT INTO city_entity (id, name) VALUES (57,'Inírida ');
+INSERT INTO city_entity (id, name) VALUES (58,'San Felipe');
+INSERT INTO city_entity (id, name) VALUES (59,'Puerto Colombia');
+INSERT INTO city_entity (id, name) VALUES (60,'La Guadalupe');
+INSERT INTO city_entity (id, name) VALUES (61,'Cacahual');
+
 INSERT INTO department_entity(id,name)VALUE(16,'Guaviare');
+INSERT INTO city_entity (id, name) VALUES (62,'San José del Guaviare');
+INSERT INTO city_entity (id, name) VALUES (63,'Calamar');
+INSERT INTO city_entity (id, name) VALUES (64,'El Retorno');
+INSERT INTO city_entity (id, name) VALUES (65,'Miraflores');
+
+
+
+
 INSERT INTO department_entity (id, name) VALUES (17,'Huila');
 INSERT INTO department_entity (id, name) VALUES (18,'La Guajira');
 INSERT INTO department_entity (id, name) VALUES (19,'Magdalena');
